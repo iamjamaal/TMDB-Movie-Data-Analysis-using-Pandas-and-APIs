9b6b7f0e05e094a9b24c77c502532a96ad368719
Noah Jamal Nabila <noah.nabila@amalitech.com>
2025-12-09 10:46:10 +0000

    tracking directories

--- config.py
+++ config.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv('7884a11d22079309e268ac5ae881da5e')
-BASE_URL = 'https://api.themoviedb.org/'
-MOVIE_IDS = [0, 299534, 19995, 140607, 299536, 597, 135397, 
+# Load API key from .env file
+API_KEY = os.getenv('TMDB_API_KEY')
+
+BASE_URL = 'https://api.themoviedb.org/3'
+
+MOVIE_IDS = [299534, 19995, 140607, 299536, 597, 135397, 
              420818, 24428, 168259, 99861, 284054, 12445, 
              181808, 330457, 351286, 109445, 321612, 260513]
